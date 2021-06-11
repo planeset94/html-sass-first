31288a227eab2bd6fedd1b323fe502406f2388f4
planeset94 <planeset94@gmail.com>
2021-06-11 18:17:34 +0200

    Dist

--- my-app/webpack.mix.js
+++ my-app/webpack.mix.js
@@ -2,6 +2,6 @@
 
 let mix = require('laravel-mix');
 
-mix.js('src/app.js', 'dist')
-    .sass('src/app.scss', 'dist')
+mix.js('src/app.js', 'js')
+    .sass('src/app.scss', 'css')
     .setPublicPath('dist');
